47e77822ed4a1a68bd41bca7dd428415a2d84e41
Noru-0 <htphat22@clc.fitus.edu.vn>
2024-12-21 00:17:40 +0700

    Update from main repo

--- components/auth/auth.models.js
+++ components/auth/auth.models.js
@@ -1,2 +1,25 @@
-const userModel = require('../../components/account/account.model');
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const userDBConnection = require('../../config/userDBConnection');
+const schema = mongoose.Schema;
+
+const userSchema = new schema({
+    username: String,
+    email: String,
+    password: String,
+    role: {
+        type: String,
+        default: 'user'
+    },
+    isActive: { type: Boolean, default: false },
+    activationToken: { type: String },
+    activationExpires: { type: Date } // Expiry time
+});
+userSchema.methods.getVerificationToken = function () {
+    const token = crypto.randomBytes(32).toString('hex');
+    this.activationToken = crypto.createHash('sha256').update(token).digest('hex');
+    this.activationExpires = Date.now() + 10 * 60 * 1000; // 10 minutes
+    return token;
+}
+const userModel = userDBConnection.model('user', userSchema);
 module.exports = userModel;
--- components/auth/auth.routes.js
+++ components/auth/auth.routes.js
@@ -6,6 +6,12 @@ const authController = require('./auth.controllers');
 router.get("/login", authController.renderLogin);
 router.post('/login', authController.login);
 
+// Forgot password route
+router.get("/forgot-password", authController.renderForgotPassword);
+router.post('/forgot-password', authController.forgotPassword);
+// Reset password route
+router.get("/reset-password", authController.renderResetPassword);
+router.post('/reset-password', authController.resetPassword);
 // Register route
 router.get("/register", authController.renderRegister);
 router.post('/register', authController.register);
@@ -13,4 +19,8 @@ router.post('/register', authController.register);
 //Logout route
 router.get("/logout", authController.logout);
 
-module.exports = router;
+// Route to check availability of username and email
+router.post("/check-availability", authController.checkAvailability);
+
+// route to verify email
+router.get("/verify", authController.verifyEmail);
